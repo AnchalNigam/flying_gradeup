94a404aa508496b8f32ae6deae9ac4c77a2f3223
Anchal Nigam <anchal.nigam@gradeup.co>
2020-11-07 11:59:26 +0530

    api

--- src/App.js
+++ src/App.js
@@ -18,21 +18,27 @@ const App = () => {
     rank: 10
     }
   };
-
+  const [resp, setResp] = useState({});
   useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
     fetch('https://spotty-skunk-16.loca.lt/fetch_leaderboard', {
       method: 'POST',
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        "userid": 121,
-        "username":"sami",
-        "level": 3,
-        "score": 30
+        "userid": urlParams.get('userid'),
+        "username": urlParams.get('userName'),
+        "level": urlParams.get('level'),
+        "score": urlParams.get('score'),
       })
     })
-    .then(resp => console.log(resp));
+    .then((response) => response.json())
+    .then(resp => {
+      setResp(resp);
+      console.log(resp)
+    });
   });
 
   return (
@@ -43,21 +49,21 @@ const App = () => {
         <div className="border text-align border-radius flex flex-row pad-lr-30 pad-tb-15 justify-between top-margin-15">
           <div className="flex flex-column">
             <span className="gray-text-color f6-3">Rank</span>
-            <span className="top-margin-10 bold f5-7">{data.me.rank}</span>
+            <span className="top-margin-10 bold f5-7">{resp.userRank}</span>
           </div>
           <div className="flex flex-column">
             <span className="gray-text-color f6-3">Your score</span>
-            <span className="top-margin-10 bold f5-7">{data.me.score}</span>
+            <span className="top-margin-10 bold f5-7">{resp.userTotalScore[0].userTotalscore}</span>
           </div>
         </div>
       </div>
       <div className="flex flex-column top-margin-50">
         <span className="f5">Top Scorer</span>
         <div className="border text-align border-radius flex flex-row pad-lr-30 pad-tb-15 justify-between top-margin-15">
-          {data.topScorer.map((score, idx) => (
+          {resp.topScorer.map((score, idx) => (
              <div className="flex flex-column">
-              <span className="gray-text-color f6-3 text-capitialize">{score.name}</span>
-              <span className="top-margin-10 bold f5-7">{score.score}</span>
+              <span className="gray-text-color f6-3 text-capitialize">{score.username}</span>
+              <span className="top-margin-10 bold f5-7">{score.sum_score}</span>
            </div>
           ))}
         </div>
